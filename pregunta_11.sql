7e519730e843d4443a6058afb4ac59047d87e83b
crrojasmazo <71391747+crrojasmazo@users.noreply.github.com>
2022-10-17 15:45:27 -0500

    LISTOX2

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,4 +38,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT strftime('%Y', c14) AS "Year", COUNT(c14) as CANT FROM tbl1 WHERE strftime('%Y', c14) = '2018'
+SELECT strftime('%Y', c14) AS "YEAR", COUNT(c14) as CANT FROM tbl1 WHERE strftime('%Y', c14) = '2018'
